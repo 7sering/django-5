37ce803903c4853864426e0e8547628ef5f26f70
coder <codenow8@gmail.com>
2024-04-07 14:21:04 +0545

    agency model &  fetched data in frontend

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import GeneralInfo
+
 
 # Register your models here.
+@admin.register(GeneralInfo)
+class GeneralInfoAdmin(admin.ModelAdmin):
+    list_display = [
+        "agency_name",
+        "location",
+        "email",
+        "phone",
+    ]
--- agency/views.py
+++ agency/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from agency.models import GeneralInfo
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "home.html")
+    general_info = GeneralInfo.objects.first()
+    context = {
+        "agency_name": general_info.agency_name,
+        "location": general_info.location,
+        "email": general_info.email,
+        "phone": general_info.phone,
+        "twitter": general_info.twitter_url,
+        "open_hours": general_info.open_hours,
+    }
+    return render(request, "home.html", context)
